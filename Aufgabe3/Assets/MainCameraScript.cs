8563621ba244e0f919eef078bf06fde87c0d06e2
rrrrrrrobin <robinfakler@hotmail.de>
2017-07-13 18:59:33 +0200

    Updated Aufgabe3
    Added teapod model and MainCameraScript

---
+++ Aufgabe3/Assets/MainCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour {
+    public Material material;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;   //get camera depth
+	}
+	
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
