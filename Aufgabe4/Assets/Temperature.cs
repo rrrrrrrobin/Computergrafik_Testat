d5529e2ca578c953f851bb7de21788d69f525b72
rrrrrrrobin <robinfakler@hotmail.de>
2017-07-23 22:34:30 +0200

    Updated Aufgabe4
    Updated formula from given script

--- Aufgabe4/Assets/Temperature.cs
+++ Aufgabe4/Assets/Temperature.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Temperature : MonoBehaviour {
 
-    static float bolzm = 5.67f * Mathf.Pow(10, -8);
+    static float bolzm = 5.67f * Mathf.Pow(10, -8); // bolzmann formula from given exercise
     float temp;
     float energy;
     float radius;
+    float volume;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,7 @@ public class Temperature : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        volume = (4 / 3) * Mathf.PI * radius;  // volume of sphere
+        energy = volume * Mathf.Pow(temp, 4) * bolzm;  // energy formula from given exercise
+    }
 }
