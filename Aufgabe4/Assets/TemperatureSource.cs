f953c71348e3acc839a079dee2a436d167dabe4d
rrrrrrrobin <robinfakler@hotmail.de>
2017-07-23 22:17:39 +0200

    Updated Aufgabe4
    Edited TemperatureSource Script

--- Aufgabe4/Assets/TemperatureSource.cs
+++ Aufgabe4/Assets/TemperatureSource.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class TemperatureSource : MonoBehaviour {
 
-    public GameObject Teapot;
-    public GameObject Tree;
+    public GameObject Teapot; // Source of the temperatur
 
     // Use this for initialization
     void Start () {
@@ -13,13 +12,13 @@ public class TemperatureSource : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();   // get all objects in scene
-
-        foreach (var obj in allObjects)
+        
+        if (Teapot != null)
         {
-            Vector4 tempPosition = new Vector4(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z, 1);
-            obj.GetComponent<MeshRenderer>().material.SetVector("_TemperaturePosition", tempPosition);
+            Vector4 tempPosition = new Vector4(Teapot.transform.position.x, Teapot.transform.position.y, Teapot.transform.position.z, 1);
+            this.GetComponent<MeshRenderer>().material.SetFloat("_TemperatureEnergy", Teapot.GetComponent<Temperature>().totalEnergy);
+            this.GetComponent<MeshRenderer>().material.SetVector("_TemperaturePosition", tempPosition);
+            this.GetComponent<MeshRenderer>().material.SetFloat("_Absorbtion", absorb);
         }
     }
 }
