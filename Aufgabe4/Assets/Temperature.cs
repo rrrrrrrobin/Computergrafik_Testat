79cf0f91fba1677b70fa2e594cf74c732a4dd9bc
rrrrrrrobin <robinfakler@hotmail.de>
2017-07-23 22:23:35 +0200

    Updated Aufgabe4
    Added Temperature script

---
+++ Aufgabe4/Assets/Temperature.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temperature : MonoBehaviour {
+
+    static float bolzm = 5.67f * Mathf.Pow(10, -8);
+    float temp;
+    float energy;
+    float radius;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
